228285854b861c92e591a9cbb88e43f5c4c9aed4
tnantoka <tnantoka@bornneet.com>
2017-02-13 22:06:03 +0900

    Add Graphene

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let package = Package(
         Target(name: "App", dependencies: ["AppLogic"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/ysnrkdm/Graphene", majorVersion: 2, minor: 4),
     ],
     exclude: [
         "Config",
